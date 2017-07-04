b5a067bd806322bb2358bb0020ac9381600290c6
ajaydivakaran <ajay_886@hotmail.com>
2017-07-04 18:23:12 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ read -p "Enter email id:" email
 
 export bitpwd
 export email
-: '
+
 #Install dependencies via package manager
 
 apt-get update && apt-get upgrade -y && \
@@ -25,6 +25,6 @@ apt-get update && apt-get install -y docker-ce docker-compose
 apt-get install libgconf2-4 libnss3-1d libxss1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp/chrome
 dpkg -i /tmp/chrome/google-chrome-stable_current_amd64.deb
-'
+
 su -p -c "./customize_shell.sh" $SUDO_USER 
 su -p -c "./setup_ide.sh" $SUDO_USER
